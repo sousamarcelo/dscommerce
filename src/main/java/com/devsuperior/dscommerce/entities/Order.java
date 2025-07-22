8c75300410f3d1447cb2c2d9d8baf47a649a2dbc
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-22 20:45:27 -0300

    Muitos-para-muitos com classe de associação PARTE 1

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -33,11 +36,13 @@ public class Order {
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
 	}
 
 	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
-		super();
 		this.id = id;
 		this.moment = moment;
 		this.status = status;
