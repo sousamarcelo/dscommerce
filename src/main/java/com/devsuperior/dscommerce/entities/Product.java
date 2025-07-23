90f660b6cd561744decc0b0d69fa048ff11b0b09
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-22 21:00:15 -0300

    Muitos-para-muitos com classe de associação PARTE 2

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -89,6 +90,14 @@ public class Product {
 	
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Order> getOrders(){
+		return items.stream().map(x -> x.getOrder()).toList();
 	}
 
 	@Override
