a21722199e6323e9a97a1b38c9e10d98c2a667e8
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-24 18:31:24 -0300

    Busca paginada de produtos

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommerce.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,12 @@ public class ProductController {
 	@GetMapping(value = "/{id}")
 	public ProductDTO findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
+		return dto;
+	}
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {  //Pageable ---> paginação
+		Page<ProductDTO> dto = service.findAll(pageable);
 		return dto;
 	}
 
