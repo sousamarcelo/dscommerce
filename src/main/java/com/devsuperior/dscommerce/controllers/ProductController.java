2348cd8ddbc2b8061717c71830a80496ad9660af
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-24 16:13:35 -0300

    Criando DTO, requisição GET product by id e estruturando camadas

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,27 +1,25 @@
 package com.devsuperior.dscommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 	
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 	
-	@GetMapping
-	public String teste() {
-		Optional<Product> result = repository.findById(1L);		
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return dto;
 	}
 
 }
