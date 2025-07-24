1c4e1d8f3f0ccdecf5a01b5a615c83fb395201dc
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-24 18:51:21 -0300

    Inserindo novo produto com POST

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,12 +1,12 @@
 package com.devsuperior.dscommerce.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +30,11 @@ public class ProductController {
 	public Page<ProductDTO> findAll(Pageable pageable) {  //Pageable ---> paginação
 		Page<ProductDTO> dto = service.findAll(pageable);
 		return dto;
+	}
+	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) { //@RequestBody --> instancia um dto com as informações do bory da requisição
+		return service.insert(dto);		 
 	}
 
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscommerce.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +30,19 @@ public class ProductService {
 	public Page<ProductDTO> findAll(Pageable pageable) {  //Pageable--> paginação
 		Page<Product> result  = repostory.findAll(pageable); //		
 		return 	result.map(x -> new ProductDTO(x)); // para o Page não precisa de stream, chamar o map diretamente	
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {  
+		
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		
+		entity = repostory.save(entity); //salva a variavel entity no banco utiliando o repository e ao mesmo tempo retorna
+		
+		return new ProductDTO(entity);
 	}
 }
