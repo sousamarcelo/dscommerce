8ec27dc64e46aa01c5fb328d099ae9bb31c09c20
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-02 23:35:50 -0300

    Pesquisa customizada(JPQL) com 'LIKE' de produtos criada no repository

--- src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.devsuperior.dscommerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dscommerce.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	//Consulta customizada: para usufruir o beneficion de paginação necessario utilizar a consulta com a JPQL
+	@Query("SELECT obj FROM Product obj "
+			+ "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))") 
+	Page<Product> searchByName(String name, Pageable pageable);
 
 }
