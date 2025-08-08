dc73e5daeff5ce166ccfd8d66cd6b09ef7daea55
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-07 22:56:18 -0300

    Adicionando controle de acesso por perfil e rota

--- src/main/java/com/devsuperior/dscommerce/DsCommerceDevsuperiorApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DsCommerceDevsuperiorApplication.java
@@ -1,13 +1,10 @@
 package com.devsuperior.dscommerce;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DsCommerceDevsuperiorApplication implements CommandLineRunner {
+public class DsCommerceDevsuperiorApplication {
 	
 	/*
 	@Autowired
--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ public class ProductController {
 		return ResponseEntity.ok(dto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')") //spring security somente as permissões 'ROLE_ADMIN'
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) { //@RequestBody --> instancia um dto com as informações do bory da requisição // o "Valid" é para considerar as validações do BinValidation inseridas no dto do produto 
 		dto = service.insert(dto);	
@@ -48,12 +50,14 @@ public class ProductController {
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')") //spring security somente as permissões 'ROLE_ADMIN'
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) { //o "Valid" é para considerar as validações do BinValidation inseridas no dto do produto 
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')") //spring security somente as permissões 'ROLE_ADMIN'
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
